4f36f8977364321d7799b91c70515e95b2a85445
emilybobbe <emilybobbe@gmail.com>
2019-02-17 15:08:21 -0800

    Add JS vars for radio buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
   console.log("did load");
   $("#content").submit(function(){
-    var nameInput = $("input#name").val();
-    console.log(nameInput);
-    var service = $("#service").val();
-    console.log(service);
-    var date = $("#date").val();
-    var time = $("#time").val();
+    var food = $("input:radio[name=food]:checked").val();
+    // console.log(foodInput);
+    var dessert = $("input:radio[name=dessert]:checked").val();
+    // console.log(dessert);
+    var song = $("input:radio[name=song]:checked").val();
+    // console.log(song);
+    var travel = $("input:radio[name=travel]:checked").val();
+    // console.log(dessert);
+    var activity = $("input:radio[name=activity]:checked").val();
+    // console.log(dessert);
 
-    $(".name").text(nameInput);
-    $(".service").text(service);
-    $(".date").text(date);
-    $(".time").text(time);
+    // $(".name").text(nameInput);
+    // $(".service").text(service);
+    // $(".date").text(date);
+    // $(".time").text(time);
 
-    $("#appointment").show();
+    $("#answer").show();
 
     event.preventDefault();
   })
